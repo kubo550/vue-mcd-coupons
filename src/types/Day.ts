2b366542cbd1e64d158fb630cd4b0eb85347abc4
powerty <qwercy142@gmail.com>
2021-06-03 02:39:38 +0200

    Types

---
+++ src/types/Day.ts
@@ -0,0 +1,3 @@
+type Day = "dziś" | "wczoraj" | "dwa dni temu" | "trzy dni temu" | "cztery dni temu" | "pięć dni temu" | "sześć dni temu" | "siedem dni temu";
+
+export default Day
---
+++ src/types/Socialmedia.ts
@@ -0,0 +1,8 @@
+interface Socialmedia {
+    id: string,
+    name: string,
+    img_src: string,
+    url: string
+};
+
+export default Socialmedia
